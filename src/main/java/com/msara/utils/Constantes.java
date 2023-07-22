c2668292fb4f3d540afdc7e568e4a40d132e14e3
Xxarhabia <msarabiacarrascal@gmail.com>
2023-07-22 13:56:20 -0500

    tarea pendiente de cambiar las consultas tradicionales por sp

--- src/main/java/com/msara/utils/Constantes.java
+++ src/main/java/com/msara/utils/Constantes.java
@@ -2,6 +2,8 @@ package com.msara.utils;
 
 public class Constantes {
 
+    //TODO cambiar las consultas sql tradicionales por un call para llamar al procedimiento almacenado
+
     public static final String SELECT_ID = "SELECT * FROM usuarios WHERE id=?";
     public static final String SELECT_USUARIOS = "SELECT * FROM usuarios";
     public static final String INSERT_USUARIO = "INSERT INTO usuarios(nombre, apellido, edad, telefono) VALUES(?,?,?,?)";
